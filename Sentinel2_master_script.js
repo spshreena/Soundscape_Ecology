f81deb2317a31c694177b8ec23810aba134c000c
Shreena Pyakurel <54719859+spshreena@users.noreply.github.com>
2020-01-28 13:15:15 -0500

    Update Sentinel2_master_script.js

--- Sentinel2_master_script.js
+++ Sentinel2_master_script.js
@@ -13,6 +13,7 @@ var sample = ee.FeatureCollection("users/shreenapyakurel/Sample_Sites"),
           [-71.53416964017146, 42.02124129930544],
           [-71.53416964017146, 42.69707031707116]]], null, false);
 
+
 // Function to mask clouds using the Sentinel-2 QA band.
 function maskS2clouds(image) {
   var qa = image.select('QA60')
@@ -31,13 +32,13 @@ function maskS2clouds(image) {
       .copyProperties(image, ["system:time_start"])
 }
 
-// Map the function over one year of data and take the median.
-// Load Sentinel-2 TOA reflectance data.
+// Load Sentinel-2 surface  reflectance data.
 var collection = ee.ImageCollection('COPERNICUS/S2_SR')
-    .filterDate('2019-06-01', '2019-08-31')
+    .filterDate('2019-05-01', '2019-08-31')
     // Pre-filter to get less cloudy granules.
     .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 10))
     .map(maskS2clouds)
+    .filterBounds(AOI)
 
 var composite = collection.median()
 
@@ -50,7 +51,8 @@ print (image);
 
 //clip image to a bounding box with all points 
 var imgclip = image.clip(AOI)
-Map.addLayer(composite, {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.3}, 'RGB', true)
+Map.addLayer(composite, {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.3}, 'RGB', false)
+Map.addLayer(composite, {bands: ['B8', 'B4', 'B3'], min: 0, max: 0.3}, 'fcc', false)
 
 //Add in sample points--need to figure how to distuinguish each--
 
@@ -60,33 +62,44 @@ Map.addLayer(sample, {color: 'yellow'}, 'sample');
 var buff3000 = sample.map(function(f) {
   return f.buffer(3000, 1); // Note that the errorMargin is set to 100.
 });
-Map.addLayer(buff3000, {color: 'green'}, '1000buff', true);
+Map.addLayer(buff3000, {color: 'green'}, '3000buff', true);
 
 // NDVI of site
 var ndvi = imgclip.normalizedDifference(['B8', 'B4']);
-var ndviParams =  {min: -0.2, max: 0.8, palette: 'FFFFFF, CE7E45, DF923D, F1B555, FCD163, 99B718, 74A901, 66A000, 529400,' +
-    '3E8601, 207401, 056201, 004C00, 023B01, 012E01, 011D01, 011301'};
-  /*{
+var ndviParams =  /*{min: -0.2, max: 0.8, palette: 'FFFFFF, CE7E45, DF923D, F1B555, FCD163, 99B718, 74A901, 66A000, 529400,' +
+    '3E8601, 207401, 056201, 004C00, 023B01, 012E01, 011D01, 011301'};*/
+  {
   min: 0.0,
   max: 1.0,
   palette: [
     'blue', 'white', 'green'
   ],
-};*/
-Map.addLayer(ndvi, ndviParams, 'NDVI image', true);
+};
+Map.addLayer(ndvi, ndviParams, 'NDVI image', false);
+
 
 //NDBI
 var ndbi = imgclip.normalizedDifference(['B11', 'B8']);
 
 // Display the result.
-var ndbiParams = {min: -1, max: 0,  palette: ['blue', 'blue', 'grey'
+var ndbiParams = {min: -1, max: 0,  palette: ['blue', 'purple', 'grey'
 
   ],
 };
-Map.addLayer(ndbi, ndbiParams, 'NDBI image', true);
+Map.addLayer(ndbi, ndbiParams, 'NDBI image', false);
 
 print(ndbi, "ndbi")
 
+//clip ndvi and ndbi to sample sites (buffer of 3000m)
 
+var fcc_clip = composite.clip(buff3000)
+Map.addLayer(fcc_clip, {bands: ['B8', 'B4', 'B3'], min: 0, max: 0.3}, 'fcc_clip', true)
 
+var ndviimage = ndvi.clip(buff3000);
+Map.addLayer(ndviimage, ndviParams, 'NDVI clip', true);
 
+var ndbiimage = ndbi.clip(buff3000);
+Map.addLayer(ndbiimage, ndbiParams, 'NDBI clip', true);
+
+var tcc_clip = composite.clip(buff3000)
+Map.addLayer(tcc_clip, {bands: ['B4', 'B3', 'B2'], min: 0, max: 0.3}, 'clip_RGB', true)
