4a4cd96f4a9642918cd25b58b3d0fd02234975a0
Shreena Pyakurel <54719859+spshreena@users.noreply.github.com>
2020-02-06 22:03:25 -0500

    Update Linear_Regressions.R

--- Linear_Regressions.R
+++ Linear_Regressions.R
@@ -1,51 +1,40 @@
-#Feb/4/2020
-#Exploring relationship between envt factors and sound ecology 
-
-#Libraries
-library(tidyverse)
-library(tidyselect)
-library(soundecology)
-library(tuneR)
-library(seewave)
-
-# import CSV
-df_evt<- read.csv('D:/RA-withSangermano/SpatialData/NDVI_NDBI_PERBUFFER.csv')
-ACI<- read.csv('D:/RA-withSangermano/SpatialData/ACI_BMB_2000-12000.csv')
-ADI <- read.csv('D:/RA-withSangermano/SpatialData/acoustic_diversity1.csv')
-NDSI_BMB <- read.csv('D:/RA-withSangermano/SpatialData/NDSI_BMB_default.csv')
-#AEI<- read.csv('D:/RA-withSangermano/SpatialData/soundfile_AEI_clean.csv')
-BI<- read.csv('D:/RA-withSangermano/SpatialData/soundfile_BI_removed_clean.csv')
+df_evt<- read.csv('D:/RA-withSangermano/CSV/NDVI_NDBI_PERBUFFER.csv')
+ACI<- read.csv('D:/RA-withSangermano/CSV/ACI_BMB_2000-12000.csv')
+ADI <- read.csv('D:/RA-withSangermano/CSV/acoustic_diversity1.csv')
+#NDSI_BMB <- read.csv('D:/RA-withSangermano/CSV/NDSI_BMB_default.csv')
+AEI<- read.csv('D:/RA-withSangermano/CSV/soundfile_AEI_clean.csv')
+BI<- read.csv('D:/RA-withSangermano/CSV/soundfile_BI_removed_clean.csv')
 
 #------------------------------------------------------------------------------------------
 #Acoustic data cleanup
 #create new data frame with only acoustic index 
 ACI_select<- ACI$LEFT_CHANNEL
 ADI_select<- ADI$LEFT_CHANNEL
-NDSI_BMB_select<- NDSI_BMB$LEFT_CHANNEL
+AEI_select<- AEI$aei_left
 BI_select<- BI$left_area
 filenames<- ACI$FILENAME
-#ADD AEI 
+
 #ADD NDSI, Biophony, Antrophony 
-#add missing points
+
 #create Acoustic dataframe
-df_sounds<- data.frame(filenames, ACI_select, ADI_select, NDSI_BMB_select, BI_select )
+df_sounds<- data.frame(filenames, ACI_select, ADI_select, AEI_select, BI_select )
 
 BP <- 'BP'
 dat_BP <- subset(df_sounds, grepl(BP, filenames))
 
 #select by site
-names <- c('BMB', 'BP', 'CC', 'FR', 'L', 'PM', 'RB', 'W_','WM')
+names <- c('BMB', 'BP', 'CC', 'FR', 'L_', 'PM', 'RB', 'W_','WM', 'EL', 'LM')
 
 for (i in length (df_sounds)){
   if (names == names[1]){
     dat_BMB <- subset(df_sounds, grepl(names[1], filenames))
   }else {
     dat_BP <- subset(df_sounds, grepl(names[2], filenames)) 
-  }else {
+  }else{
     dat_CC <- subset(df_sounds, grepl(names[3], filenames))
-  }else {
+  }else{
     dat_FR <- subset(df_sounds, grepl(names[4], filenames))
-  }else {
+  }else{
     dat_L <- subset(df_sounds, grepl(names[5], filenames))
   }else {
     dat_PM <- subset(df_sounds, grepl(names[6], filenames))
@@ -55,8 +44,14 @@ for (i in length (df_sounds)){
     dat_W <- subset(df_sounds, grepl(names[8], filenames))
   }else{
     dat_WM <- subset(df_sounds, grepl(names[9], filenames))
+  }else{
+    dat_EL <- subset(df_sounds, grepl(names[10], filenames))
+  }else{
+    dat_LM <- subset(df_sounds, grepl(names[11], filenames))
   }
 }
+
+#NEED TO FIX dat_EL it has double values************************************************
 #mean acoustic index per site
 mean_BMB <- sapply(dat_BMB, mean)
 mean_BP <- sapply(dat_BP, mean)
@@ -71,3 +66,15 @@ mean_WM <- sapply(dat_WM, mean)
 #data frame
 df_sound_mean<- data.frame(mean_BMB, mean_BP, mean_CC, mean_FR, mean_L, mean_PM, mean_RB, mean_W, mean_WM )
 #---------------------------------------------------------------------------------------------
+#Enviornmental Factors
+#DATA FRAME per buffer
+m500<- select(filter(df_evt, Buffer_Meter == 500),c(SITE, MEAN_NDVI, MEAN_NDBI))
+m1000<- select(filter(df_evt, Buffer_Meter == 1000),c(SITE, MEAN_NDVI, MEAN_NDBI))
+m1500<- select(filter(df_evt, Buffer_Meter == 1500),c(SITE, MEAN_NDVI, MEAN_NDBI))
+m2000<- select(filter(df_evt, Buffer_Meter == 2000),c(SITE, MEAN_NDVI, MEAN_NDBI))
+m2500<- select(filter(df_evt, Buffer_Meter == 2500),c(SITE, MEAN_NDVI, MEAN_NDBI))
+m3000<- select(filter(df_evt, Buffer_Meter == 3000),c(SITE, MEAN_NDVI, MEAN_NDBI))
+#--------------------------------------------------------------------------------------------------
+#Linear Regressions 
+
+
